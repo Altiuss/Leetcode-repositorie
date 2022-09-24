1766f3e2fb6d8837bdd08317cec18625817b054c
Aigars Alutis <101283354+Altiuss@users.noreply.github.com>
2022-09-24 21:15:01 +0200

    Time: 616 ms (10.35%), Space: 43 MB (92.10%) - LeetHub

---
+++ 136-single-number/136-single-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+for (var i = 0; i < nums.length; i++) {
+    let a = 0;
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        a++;
+      }
+    }
+    if (a === 1) {
+      let rez = nums[i];
+     return rez;
+    }
+  }
+   
+};
